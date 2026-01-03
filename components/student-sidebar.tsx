320a09c974dd8e775124e625df096b15f21b757b
nopianpdlh <novianfadhilah03@gmail.com>
2026-01-03 22:41:10 +0700

    sync-invoice-status

--- components/student-sidebar.tsx
+++ components/student-sidebar.tsx
@@ -242,7 +242,9 @@ export function StudentSidebar({
                   className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={user.avatar} alt={user.name} />
+                    {user.avatar && (
+                      <AvatarImage src={user.avatar} alt={user.name} />
+                    )}
                     <AvatarFallback className="rounded-lg bg-primary text-primary-foreground">
                       {getInitials(user.name)}
                     </AvatarFallback>
